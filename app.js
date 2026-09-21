e9d578d2968c712276dc2310607f8804c7c92fdc
lucasduve <lucas_duve@estudante.sesisenai.org.br>
2026-09-21 20:47:03 -0300

    Adiciona novos complementos

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const nome = "Lucas";
+const idade = 16;
 const cidade = "São Paulo";
 const atividade = "jogos";
 const aprender = "desenvolvimento de aplicativos";
 const area = "tecnologia";
 const mensagem = "Estou evoluindo na programação!";
+const desafio = "Criar um aplicativo completo do zero";
 
 console.log("===================================");
 console.log("          MEU APLICATIVO");
 console.log("===================================");
 console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
 console.log(`Cidade: ${cidade}`);
 console.log(`Atividade de interesse: ${atividade}`);
 console.log(`Quero aprender: ${aprender}`);
 console.log(`Área de interesse: ${area}`);
 console.log(`Mensagem: ${mensagem}`);
+console.log(`Desafio: ${desafio}`);
