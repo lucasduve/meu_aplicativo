352fe17ca9c6aef659d910fda29fb92c570ed333
lucasduve <lucas_duve@estudante.sesisenai.org.br>
2026-09-21 20:40:56 -0300

    Cria aplicativo

---
+++ app.js
@@ -0,0 +1,16 @@
+const nome = "Lucas";
+const cidade = "São Paulo";
+const atividade = "jogos";
+const aprender = "desenvolvimento de aplicativos";
+const area = "tecnologia";
+const mensagem = "Estou evoluindo na programação!";
+
+console.log("===================================");
+console.log("          MEU APLICATIVO");
+console.log("===================================");
+console.log(`Nome: ${nome}`);
+console.log(`Cidade: ${cidade}`);
+console.log(`Atividade de interesse: ${atividade}`);
+console.log(`Quero aprender: ${aprender}`);
+console.log(`Área de interesse: ${area}`);
+console.log(`Mensagem: ${mensagem}`);
